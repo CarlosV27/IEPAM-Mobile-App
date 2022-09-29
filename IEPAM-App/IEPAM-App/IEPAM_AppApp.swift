320fd2cc0cfd7355de10125d5c7f6dcc72c7ec63
CarlosV27 <a00832076@tec.mx>
2022-09-29 11:45:47 -0500

    Delete old app
    New App with core data included

---
+++ IEPAM-App/IEPAM-App/IEPAM_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  IEPAM_AppApp.swift
+//  IEPAM-App
+//
+//  Created by Alumno on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct IEPAM_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
